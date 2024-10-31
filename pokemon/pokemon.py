627b93b604734c468fe177d42287a1102c8fa593
Ignacio Marín <84554799+Igna24@users.noreply.github.com>
2024-10-31 09:29:45 -0600

    Pokemon game (#2)
    * add utils file
    
    * first commit for pokemon game

---
+++ pokemon/pokemon.py
@@ -0,0 +1,56 @@
+class Pokemon:
+    def __init__(self, name, pokemonType, level, healthPoints, attackPoints):
+        self.name = name
+        self.pokemonType = pokemonType
+        self.level = level
+        self.healthPoints = healthPoints
+        self.attackPoints = attackPoints
+
+    def attack(self, otherPokemon):
+        """
+        Realiza un ataque al otro pokemon y ajusta sus puntos de salud
+        """
+        print(f"{self.name} ataca a {otherPokemon.name} con {self.attackPoints} puntos de ataque")
+
+        if self.attackPoints < otherPokemon.healthPoints / 2:
+            otherPokemon.healthPoints -= 1
+            print(f"el ataque es bajo. {otherPokemon.name} pierde 1 punto de salud")
+        elif self.attackPoints >= otherPokemon.healthPoints:
+            otherPokemon.healthPoints -= int(otherPokemon.healthPoints * 0.2)
+            print(f"el ataque es fuerte. {otherPokemon.name} pierde el 20% de su salud")
+        else:
+            damage = max(2, (self.attackPoints - otherPokemon.healthPoints // 2) // 2)
+            otherPokemon.healthPoints -= damage
+            print(f"el ataque es medio. {otherPokemon.name} pierde {damage} puntos de salud")
+
+        # evitar que la salud sea negativa
+        if otherPokemon.healthPoints < 0:
+            otherPokemon.healthPoints = 0
+
+        print(f"salud de {otherPokemon.name} ahora es {otherPokemon.healthPoints}")
+
+    def levelUp(self):
+        """
+        Sube de nivel y mejora los atributos de salud y ataque
+        """
+        self.level += 1
+        self.healthPoints += 2
+        self.attackPoints += 3
+        print(f"{self.name} sube al nivel {self.level}. salud aumentada a {self.healthPoints}, ataque aumentado a {self.attackPoints}")
+
+pokemon1 = Pokemon("pokemon1", "elctrico", 5, 30, 15)
+pokemon2 = Pokemon("pokemon2", "fuego", 6, 35, 10)
+
+print("estado inicial de los pokemones :")
+print(f"{pokemon1.name} - tipo: {pokemon1.pokemonType}, nivel : {pokemon1.level}, salud: {pokemon1.healthPoints}, ataque: {pokemon1.attackPoints}")
+print(f"{pokemon2.name} - tipo: {pokemon2.pokemonType}, nivel: {pokemon2.level}, salud : {pokemon2.healthPoints}, ataque : {pokemon2.attackPoints}\n")
+
+pokemon1.attack(pokemon2)
+pokemon2.attack(pokemon1)
+
+pokemon1.levelUp()
+pokemon2.levelUp()
+
+print("\nestado final de los pokemones:")
+print(f"{pokemon1.name} - tipo : {pokemon1.pokemonType}, nivel: {pokemon1.level}, salud: {pokemon1.healthPoints}, ataque: {pokemon1.attackPoints}")
+print(f"{pokemon2.name} - tipo: {pokemon2.pokemonType}, nivel: {pokemon2.level}, salud: {pokemon2.healthPoints}, ataque : {pokemon2.attackPoints}")
